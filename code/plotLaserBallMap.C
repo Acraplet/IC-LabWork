bddaf482cd8f682d5dd716c9edbd6ec486f40ef3
Test <Test@test>
2026-01-06 11:18:05 +0000

    Minor documentation added

--- code/plotLaserBallMap.C
+++ code/plotLaserBallMap.C
@@ -58,6 +58,7 @@ void plotLaserBallMap() {
 
     };
 
+    //This is 3sigma away from pedestal 
     std::vector<double> pmtIntLims = {
         0.0021896,
         0.0031439,
@@ -239,7 +240,7 @@ void plotLaserBallMap() {
                 double value_Monitor;
                 std::cout << pmtDirs_raw[ip].c_str() << " " << tree_name[ip] << std::endl;
 
-
+                //PMT branch name
                 const char* brName = pmtDirs_raw[ip].c_str();
 
                 TBranch* br = T->GetBranch(brName);
